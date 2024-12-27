2caee6fe70dfe9ebc1907a3a6a2cbee2d4ae43d9
VIKING <viking.yfy@qq.com>
2024-12-27 23:55:41 +0800

    Update QCA Source

--- package/base-files/files/etc/uci-defaults/990_set-wireless.sh
+++ package/base-files/files/etc/uci-defaults/990_set-wireless.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 . /usr/share/libubox/jshn.sh
 
-# 获取无线设备的数量
-RADIO_NUM=$(uci show wireless | grep -c "wifi-device")
-
-# 如果没有找到无线设备，直接退出
-if [ "$RADIO_NUM" -eq 0 ]; then
-	exit 0
-fi
-
 # 默认WIFI设置
 BASE_SSID='OWRT'
 BASE_WORD='12345678'
 
-# 配置无线设备
+# 获取无线设备的数量
+RADIO_NUM=$(uci show wireless | grep -c "wifi-device")
+
+# 如果没有找到无线设备，直接退出
+[ "$RADIO_NUM" -eq 0 ] && exit 0
+
+# 配置参数
 configure_wifi() {
 	local radio=$1
 	local channel=$2
@@ -28,16 +26,17 @@ configure_wifi() {
 	fi
 
 	# 设置无线设备参数
-	uci set wireless.radio${radio}.channel="${channel}"
-	uci set wireless.radio${radio}.htmode="${htmode}"
+
+	uci set wireless.radio${radio}.channel=${channel}
+	uci set wireless.radio${radio}.htmode=${htmode}
 	uci set wireless.radio${radio}.country='CN'
 	uci set wireless.radio${radio}.disabled='0'
 	uci set wireless.radio${radio}.cell_density='0'
 	uci set wireless.radio${radio}.mu_beamformer='1'
 
-	uci set wireless.default_radio${radio}.ssid="${ssid}"
+	uci set wireless.default_radio${radio}.ssid=${ssid}
 	uci set wireless.default_radio${radio}.encryption='psk2+ccmp'
-	uci set wireless.default_radio${radio}.key="${BASE_WORD}"
+	uci set wireless.default_radio${radio}.key=${BASE_WORD}
 	uci set wireless.default_radio${radio}.ieee80211k='1'
 	uci set wireless.default_radio${radio}.time_advertisement='2'
 	uci set wireless.default_radio${radio}.time_zone='CST-8'
@@ -46,65 +45,70 @@ configure_wifi() {
 	uci set wireless.default_radio${radio}.wnm_sleep_mode_no_keys='1'
 }
 
-# 设置无线设备的默认配置
-FIRST_5G=''
-set_wifi_default_config() {
-	local radio=$1
-	local htmode=$2
-	local band=$(uci get wireless.radio${radio}.band)
-	local channel
-
-	if [ "$band" = '5g' ]; then
-		channel=149
-		if [ "$htmode" = 'HE160' ] || [ "$htmode" = 'VHT160' ]; then
-			channel=44
+# 查询mode
+query_mode() {
+	json_load_file "/etc/board.json"
+	json_select wlan
+	json_get_keys phy_keys
+	for phy in $phy_keys; do
+		json_select $phy
+		json_get_var path_value "path"
+		if [ "$path_value" = "$1" ]; then
+			json_select info
+			json_select bands
+			json_get_keys band_keys
+			for band in $band_keys; do
+				json_select $band
+				json_select modes
+				json_get_keys mode_keys
+				for mode in $mode_keys; do
+					json_get_var mode_value $mode
+					last_mode=$mode_value
+				done
+				json_select ..
+				json_select ..
+			done
+			echo "$last_mode"
+			return
 		fi
-		if [ -z "$FIRST_5G" ]; then
-			if [ "$RADIO_NUM" -eq 2 ]; then
-				configure_wifi "$radio" "$channel" "$htmode" "${BASE_SSID}-5G"
-			else
-				configure_wifi "$radio" "$channel" "$htmode" "${BASE_SSID}-5G_1"
-			fi
-			FIRST_5G='1'
-		else
-			configure_wifi "$radio" "$channel" "$htmode" "${BASE_SSID}-5G_2"
-		fi
-	else
-		configure_wifi "$radio" '6' "$htmode" "${BASE_SSID}"
-	fi
-}
-
-# 读取 /etc/board.json 文件
-json_load_file /etc/board.json
-
-# 提取 WLAN 信息
-json_select wlan
-
-# 遍历所有 PHY 接口
-id=0
-json_get_keys phy_keys
-for phy in $phy_keys; do
-	json_select $phy
-	json_select info
-	json_select bands
-	json_get_keys band_keys
-	for band in $band_keys; do
-		json_select $band
-		json_select modes
-		json_get_keys mode_keys
-		last_mode=""
-		for mode in $mode_keys; do
-			json_get_var mode_value $mode
-			last_mode=$mode_value
-		done
-		set_wifi_default_config $id $last_mode
-		json_select ..
-		json_select ..
 		json_select ..
 	done
-	json_select ..
-	json_select ..
-	id=$((id + 1))
+}
+
+# 设置无线设备的默认配置
+FIRST_5G=''
+set_wifi_def_cfg() {
+	local band=$(uci get wireless.radio${1}.band)
+	local path=$(uci get wireless.radio${1}.path)
+	local htmode=$(query_mode "$path")
+	local channel=6
+	local WIFI_SSID="$BASE_SSID"
+
+	case "$band" in
+	'5g')
+		channel=149
+		[ "$htmode" = 'HE160' ] || [ "$htmode" = 'VHT160' ] && channel=44
+		if [ -z "$FIRST_5G" ]; then
+			[ "$RADIO_NUM" -gt 2 ] && WIFI_SSID="${BASE_SSID}-5G_1" || WIFI_SSID="${BASE_SSID}-5G"
+			FIRST_5G=1
+		else
+			WIFI_SSID="${BASE_SSID}-5G_2"
+		fi
+		;;
+	*)
+		case "$htmode" in
+		'HT40' | 'VHT40' | 'HE40')
+			htmode="${htmode%40}20"
+			;;
+		esac
+		;;
+	esac
+
+	configure_wifi "$1" "$channel" "$htmode" "$WIFI_SSID"
+}
+
+for i in $(seq 0 $((RADIO_NUM - 1))); do
+	set_wifi_def_cfg "$i"
 done
 
 # 提交配置并重启网络服务
